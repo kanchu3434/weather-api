0d709f8d178cb07965c816b058fd996dcefd2af7
kanchan <kanchankotkar001@gmail.com>
2022-12-23 06:37:40 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import ImgGoogleMap from './google-maps.png';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [city, setCity] = useState("");
+  const [temp, setTemp] = useState("0");
+  const [description, setDescription] = useState("");
 
+  useEffect(() => {
+    async function loadData() {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3d4707888cae1cef35d3b4069b3862d3`);
+
+      if (response.status === 200) {
+        const temp = console.log(response.data.main.temp - 273.15);
+        setTemp(temp)
+      }
+
+    }
+  
+   
+    loadData();
+  }, [city])
+
+  return (
+    <>
+      <div className="weather-container">
+        <h1 className='app-title'> Weather APP</h1>
+        <div className="search-container" >
+          <img src={ImgGoogleMap} className="img-location" />
+          <input type="text" 
+            placeholder="Enter city name"
+            className="input-city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)} />
+        </div>                                                  
+        <div className="output-city">
+          <h3>City:{city}</h3>
+          
+        </div>
+
+        <div className="output-city">
+        <h3>Temperature:{temp}</h3>
+          
+        </div>
+      </div>
+    </>
+  )
+
+}
 export default App;
